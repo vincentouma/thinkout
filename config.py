1c5c475a9e457f9abb3db71addf2db16986b71f9
vincentouma <vinceoumah@gmail.com>
2019-08-04 21:43:20 +0300

    creat app instance and inititializing flask extention

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
 import os
+
+class Config:
+    """
+    This is the parent class which will have the general configurations
+    """
+
+
+
+
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
