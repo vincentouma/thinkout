06cbd159c9ea55c9a7397ccab1feabb42389550c
vincentouma <vinceoumah@gmail.com>
2019-08-04 21:59:12 +0300

    register blue print

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,25 @@ def create_app(config_name):
 
 # Creating the app configurations
 app.config.from_object(config_options[config_name])
+
 # Initializing flask extensions
 bootstrap.init_app(app)
 db.init_app(app)
+
+
+
+
+# Registering the blueprint
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    
+configure_uploads(app,photos)
+
 
 
 
