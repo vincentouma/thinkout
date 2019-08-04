a266b87784ff1584c5b31362aa84cb54754feb4d
vincentouma <vinceoumah@gmail.com>
2019-08-05 01:11:38 +0300

    add auth forms

--- app/main/views.py
+++ app/main/views.py
@@ -24,3 +24,30 @@ def categories(category):
         pitches = Pitch.query.filter_by(category = category).all()
 
     return render_template("pitches.html", pitches = pitches, title = category)
+
+
+
+
+    @main.route("/<uname>/add_pitch", methods = ["GET","POST"])
+@login_required
+def add_pitch(uname):
+    form = AddPitchForm()
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+    title = "Add Pitch"
+    if form.validate_on_submit():
+        title = form.title.data
+        pitch = form.pitch.data
+        category = form.category.data 
+        dateOriginal = datetime.datetime.now()
+        time = str(dateOriginal.time())
+        time = time[0:5]
+        date = str(dateOriginal)
+        date = date[0:10]
+        new_pitch = Pitch(title = title, pitch = pitch, category = category,user_id = user.id)
+        new_pitch.save_pitch()
+        pitches = Pitch.query.all()
+        return redirect(url_for("main.categories",category = category))
+    return render_template("add_pitch.html",form = form, title = title)
+
