4ed3ae068cc35f3a4f770f4e2790b1631c9a12d8
vincentouma <vinceoumah@gmail.com>
2019-08-05 03:44:18 +0300

    connect app gmail

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManagerfrom flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
 
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY='vince'
-export MAIL_USERNAME='vinceoumah@gmail.com'
-export MAIL_PASSWORD='0721729076'
+export MAIL_USERNAME='vinceobindi@gmail.com'
+export MAIL_PASSWORD='0717654230'
+# export DATABASE_URL='postgresql+psycopg2://moringa:vinceobindi1005@localhost/thinkout'
 
 python3.6 manage.py server
